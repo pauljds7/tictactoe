dab98aa30b63f7e4d6a2473a0db13953f6fe600b
Paulius Juodsnukis <paulius.juodsnukis@gmail.com>
2021-11-29 16:32:34 +0200

    Add event listeners and unifing response function.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Test test')
+/**
+ * Logic for the Tic Tac Toe game
+ */
+
+//Initialize the listeners for the 9 ttt buttons(cells)
+for (let i = 1; i <= 9; i++) {
+    document.getElementById(i).addEventListener('click',
+        () => {
+            onClick(i);
+        }
+    );
+}
+
+
+function onClick(id){
+    element = document.getElementById(id);
+    element.innerText = 'Hello';
+}
